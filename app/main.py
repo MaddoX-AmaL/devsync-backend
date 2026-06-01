55fe626fa8107773875198911873f5afef694bcb
MaddoX-Amal <amalghosh4343@gmail.com>
2026-06-01 22:41:21 +0530

    Add signup API and Pydantic schema validation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.schemas.user_schema import UserCreate
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "DevSync Backend Running"}
+
+
+@app.post("/signup")
+def signup(user: UserCreate):
+    return {
+        "message": "User created successfully",
+        "user": user
+    }
