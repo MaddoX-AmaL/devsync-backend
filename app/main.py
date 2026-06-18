974e84ae83904adb31dbb94fb3dbbc202790acf8
MaddoX-Amal <amalghosh4343@gmail.com>
2026-06-18 18:39:29 +0530

    Add login system with password verification

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from app.schemas.user_schema import UserCreate
+from app.database.database import SessionLocal
+from app.models.user_model import User
+from app.utils.security import hash_password
+from app.schemas.user_schema import UserLogin
+from app.utils.security import verify_password
 
 app = FastAPI()
 
@@ -11,7 +16,53 @@ def root():
 
 @app.post("/signup")
 def signup(user: UserCreate):
+
+    db = SessionLocal()
+
+    existing_user = db.query(User).filter(
+      User.email == user.email
+    ).first()
+
+    if existing_user:
+      return {
+        "message": "Email already registered"
+    }
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+
     return {
-        "message": "User created successfully",
-        "user": user
+        "message": "User created successfully"
+    }
+
+@app.post("/login")
+def login(user: UserLogin):
+
+    db = SessionLocal()
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not existing_user:
+        return {
+            "message": "Invalid email or password"
+        }
+
+    if not verify_password(
+        user.password,
+        existing_user.password
+    ):
+        return {
+            "message": "Invalid email or password"
+        }
+
+    return {
+        "message": "Login successful"
     }
